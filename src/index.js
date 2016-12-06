d29d875e5327c44075c859c95a3e9c79246f1141
C.T. Lin <chentsulin@gmail.com>
2016-12-06 17:02:29 +0800

    use remote-redux-devtools instead of redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable global-require */
 import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
@@ -9,12 +8,6 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
-
-if (process.env.NODE_ENV === 'development') {
-  const createDevToolsWindow = require('./utils/createDevToolsWindow').default;
-
-  createDevToolsWindow(store);
-}
 
 
 render(
