349be9302621175e1e94630c498a585fdcc287b6
C. T. Lin <chentsulin@gmail.com>
2016-12-06 18:39:08 +0800

    use react-hot-loader v3 instead of react-transform (#70)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,6 +10,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'react-hot-loader/patch',
     './src/index',
   ],
   output: {
